e435538003d4436155f3fe5cc21b0ecd5da0b677
DanBlik <blikoff.d@ya.ru>
2019-04-22 22:54:15 +0300

    Пробовал переписать код на ES5, функция renderGoodsList() не работает как должна

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-"use strict";
+/*"use strict";
 
 const goods = [{
         title: 'Shirt',
@@ -32,4 +32,49 @@ const renderGoodsList = list => {
     document.querySelector('.goods-list').innerHTML = goodsList;
 };
 
+renderGoodsList(goods);*/
+
+/*ES5 code*/
+var goods = [{
+        title: 'Shirt',
+        price: 150
+    },
+    {
+        title: 'Socks',
+        price: 50
+    },
+    {
+        title: 'Jacket',
+        price: 350
+    },
+    {
+        title: 'Shoes',
+        price: 250
+    },
+];
+
+var renderGoodsItem = function(title, price) {
+    return `<div class="goods-item">
+    <img src="./img/wear.jpg" alt="">
+    <h3>${title}</h3>
+    <p>${price}$</p>
+    <button class="buy" type="button">Купить</button>
+    </div>`;
+};
+
+console.log(renderGoodsItem());
+
+function renderGoodsList(list) {
+    var goodsList = list.forEach(function(item){
+         renderGoodsItem(item.title, item.price);
+         console.log(item);
+         console.log(item.title);
+         console.log(list);
+        });
+    document.querySelector('.goods-list').innerHTML = goodsList;
+    console.log(goodsList);
+};
+
 renderGoodsList(goods);
+
+//*/ 
