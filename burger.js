c5bdc0cbc915f352e83df9c79fb1608d0626f0f7
DanBlik <blikoff.d@ya.ru>
2019-04-26 18:20:04 +0300

    Выполнил первые 2 номера на сколько их понял, задачу с бургером еще не реализовал до конца, доделываю

---
+++ burger.js
@@ -0,0 +1,41 @@
+class Hamburger {
+    constructor (size, stuffing) { 
+        this.size = size;
+        this.stuffing = stuffing;
+    }
+
+    addTopping(topping) { // Добавить добавку 
+        let toppings = [];
+        let len = document.querySelector
+        for (let i = 0; i < )
+        if checked == true
+
+    }
+
+    removeTopping(topping) { // Убрать добавку 
+    }
+
+    getToppings(topping) { // Получить список добавок 
+    }
+
+    getSize() { // Узнать размер гамбургера 
+    }
+
+    getStuffing() { // Узнать начинку гамбургера 
+    }
+
+    calculatePrice() { // Узнать цену 
+    }
+
+    calculateCalories() { // Узнать калорийность 
+    }
+}
+
+let burger = new Hamburger();
+const fun1 = () => {
+    let cheese = document.querySelector('#cheese');
+    if (cheese.checked) {
+        
+    }
+}
+
--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-/*"use strict";
-
+'use strict';
+/*
 const goods = [{
         title: 'Shirt',
         price: 150
@@ -29,15 +29,15 @@ const renderGoodsItem = (title, price) => {
 console.log(renderGoodsItem());
 
 const renderGoodsList = list => {
-    let goodsList = list.map(item => renderGoodsItem(item.title, item.price));
+    let goodsList = list.map(item => renderGoodsItem(item.title, item.price)).join('');
     document.querySelector('.goods-list').innerHTML = goodsList;
     console.log(item);
-         console.log(item.title);
-         console.log(list);
+    console.log(item.title);
+    console.log(list);
 };
 
 renderGoodsList(goods);*/
-
+/*
 //ES5 code
 var goods = [{
         title: 'Shirt',
@@ -72,4 +72,92 @@ function renderGoodsList(list) {
 };
 
 renderGoodsList(goods);
+*/
+class goodsItem {
+    constructor(title, price) {
+        this.title = title;
+        this.price = price;
+    }
 
+    render() {
+        return `<div class="goods-item">
+        <img src="./img/wear.jpg" alt="">
+        <h3>${this.title}</h3>
+        <p>${this.price}$</p>
+        <button class="buy" type="button">Купить</button>
+        </div>`;
+    }
+}
+
+class goodsList {
+    constructor() {
+        this.goods = [];
+    }
+
+    fetchGoods() {
+        this.goods = [{
+                title: 'Shirt',
+                price: 150
+            },
+            {
+                title: 'Socks',
+                price: 50
+            },
+            {
+                title: 'Jacket',
+                price: 350
+            },
+            {
+                title: 'Shoes',
+                price: 250
+            },
+        ];
+    }
+
+    render() {
+        let listHtml = '';
+        this.goods.map(good => {
+            const goodItem = new goodsItem(good.title, good.price);
+            listHtml += goodItem.render();
+        });
+        document.querySelector('.goods-list').innerHTML = listHtml;
+    }
+
+    getSum() {
+        let sum = 0;
+        for (let i = 0; i < this.goods.length; i++){
+            sum += this.goods[i].price;
+        }
+        document.querySelector('.sumBasket').innerHTML = `Сумма всех товаров - ${sum}$`;
+    }
+}
+
+class elemOfBasket { 
+    constructor() {
+        this.title = title;
+        this.price = price;
+    }
+
+    addToBasket() {
+        //добавить товар в корзину
+    }
+
+    removeFromBasket() {
+        //удалить товар из корзины
+    }
+}
+
+class Basket {
+    constructor() {
+        var itemsBasket = []; 
+    }
+    
+    getSum() {
+
+    }
+}
+
+const list = new goodsList();
+list.fetchGoods();
+list.render();
+list.getSum();
